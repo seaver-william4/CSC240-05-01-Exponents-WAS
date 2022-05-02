b9ffb616b6bf7e27c495936980c3c0b119f0960d
seaver-william4 <wseave8945@my.sullivan.edu>
2022-05-01 20:09:01 -0400

    Created variables, the process that does the calculations, and the methods used to perform the calculations.

--- Exponents/Form1.cs
+++ Exponents/Form1.cs
@@ -8,8 +8,8 @@ using System.Text;
 using System.Windows.Forms;
 
 /**************************************
- * Name:
- * Date:
+ * Name: William Seaver
+ * Date: 5/1/2022
  * Description: A program using user-created method
  *              for the math function Square and Cube
  * Assistance:
@@ -27,38 +27,61 @@ namespace Exponents
         private void xGoButton_Click(object sender, EventArgs e)
         {
             // STORAGE
-
+            int Input = 0;
+            int Squared = 0;
+            int Cubed = 0;
             // INPUT
-
+ 
+            Input = Convert.ToInt32(xInputTextBox.Text);
+          
             // PROCESS
             /* calculate the square of the number and add it to the output - 
              * do this with a method call that allows you to pass the number
              * and then receive the answer */
+            Squared = Square(Input);
+            xOutputLabel.Text = Convert.ToString(Squared);
 
             /* calculate the cube of the number and add it to the output - 
              * do this by creating a methd as in the squaring example above */
-
+            Cubed = Cube(Input);
+            try
+            {
+                xOutputLabel.Text = Convert.ToString(Squared) + " " + Convert.ToString(Cubed);
+            }
+            catch (FormatException)
+            {
+                xOutputLabel.Text = "Please enter an integer";
+            }
             // OUTPUT
             // set the output to visible
-
+            xOutputLabel.Visible = true;
             // turn the Go button off
+            xGoButton.Enabled = false;
         }
 
         public int Square(int num)
         {
             // we will create this code in class
+            int Squared = num * num;
+            return Squared;
         }
 
         public int Cube(int num)
         {
             // we will create this code in class
+            int Cubed = num * num * num;
+            return Cubed;
         }
 
         private void xResetButton_Click(object sender, EventArgs e)
         {
             // the input and output text should be emptied
+            xInputTextBox.Text = "";
+            xOutputLabel.Text = "";
             // turn the output to invisible
+            xOutputLabel.Visible = false;
             // turn the Go button back on
+            xGoButton.Enabled = true;
         }
 
     }
